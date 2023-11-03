7597e463baa1ede57fb80eaf4aca5584c5c39fa7
Ramilxp2010 <38380515+Ramilxp2010@users.noreply.github.com>
2023-11-03 09:50:42 +0300

    cookie authentication (#4)

---
+++ QuestionAppUI/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using QuestionAppUI.Models;
+using System.Security.Claims;
+
+namespace QuestionAppUI.Areas.Identity.Account.Pages
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public AuthUserModel UserVM { get; set; }
+
+        private readonly IHttpContextAccessor _accessor;
+        public LoginModel(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = QuestionAppUI.Data.SampleData.AuthUsers.FirstOrDefault(u => u.UserName == UserVM.UserName && u.Password == UserVM.Password);
+            if (user == null)
+                return Page();
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim("ObjectIdentifier", user.Identifier)
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            try
+            {
+                await _accessor.HttpContext.SignInAsync(
+                   CookieAuthenticationDefaults.AuthenticationScheme,
+                   new ClaimsPrincipal(claimsIdentity),
+                   new AuthenticationProperties());
+
+                return Redirect("/");
+            }
+            catch 
+            {
+                return Page();
+            }
+
+        }
+    }
+}
--- QuestionAppUI/RegisterServices.cs
+++ QuestionAppUI/RegisterServices.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.IdentityModel.Tokens;
@@ -9,37 +10,22 @@ namespace QuestionAppUI
 {
     public static class RegisterServices
     {
-        public static void ConfigureServices(this WebApplicationBuilder builder) 
+        public static void ConfigureServices(this WebApplicationBuilder builder)
         {
+            builder.Services.AddHttpContextAccessor();
             builder.Services.AddAuthenticationCore();
 
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddMemoryCache();
 
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options => 
-                {
-                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidIssuer = AuthOptions.ISSUER,
-                        ValidateAudience = true,
-                        ValidAudience = AuthOptions.AUDIENCE,
-                        ValidateLifetime = true,
-                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
-                        ValidateIssuerSigningKey = true
-                    };
-                });
-
-            builder.Services.AddAuthorization(options =>
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
             {
-                options.AddPolicy("Admin", policy => 
-                {
-                    //TODO
-                    policy.RequireClaim("jobTitle", "Admin");
-                });
+                options.ExpireTimeSpan = TimeSpan.FromDays(20);
             });
+
+            builder.Services.AddAuthorization();
 
             builder.Services.AddSingleton<IDbConnection, DbConnection>();
             builder.Services.AddSingleton<ICategoryData, MongoCategoryData>();
@@ -47,17 +33,6 @@ namespace QuestionAppUI
             builder.Services.AddSingleton<IUserData, MongoUserData>();
             builder.Services.AddSingleton<IQuestionData, MongoQuestionData>();
 
-            builder.Services.AddScoped<ProtectedSessionStorage>();
-            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
         }
-    }
-
-    public class AuthOptions
-    {
-        public const string ISSUER = "MyAuthServer"; // издатель токена
-        public const string AUDIENCE = "MyAuthClient"; // потребитель токена
-        const string KEY = "mysupersecret_secretkey!123123412341234";   // ключ для шифрации
-        public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
-            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
     }
 }
